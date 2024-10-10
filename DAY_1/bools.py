61d47283f32abff2fb7f0466da04b5e26dd669a5
Thapelo Mahloko <mrtmahloko@gmail.com>
2024-10-10 13:56:56 +0200

    Finished Day tasks

---
+++ DAY_1/bools.py
@@ -0,0 +1,5 @@
+# Booleans
+# Boolean type, holds the value True or False
+x = True
+
+print(type(x))  # <class 'bool'>
---
+++ DAY_1/numbers.py
@@ -0,0 +1,10 @@
+# Working with numbers
+# Integers
+print(5)  # Output: 5
+print(-5)  # Output: -5
+# Floating point numbers
+print(3.14)  # Output: 3.14
+
+# Type function 
+print(type(5))  # Output: <class 'int'>
+print(type(3.14))  # Output: <class 'float'>
