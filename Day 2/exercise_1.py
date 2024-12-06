a60708f90a703b1c6c6f607dd674af193967754f
Thapelo Mahloko <mrtmahloko@gmail.com>
2024-12-06 22:06:03 +0200

    Day 2

---
+++ Day 2/exercise_1.py
@@ -0,0 +1,12 @@
+"""
+String Concatenation: Combine Firstname and Lastname
+"""
+# Define variables
+first_name = "Thapelo"
+last_name = "Mahloko"
+
+# String Concatenation
+full_names = first_name + last_name
+
+# Print Output
+print(full_names)
