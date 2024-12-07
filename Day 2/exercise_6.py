1e4953fb3a523f0398ad3b777fe16a5b2383348f
Thapelo Mahloko <mrtmahloko@gmail.com>
2024-12-07 13:07:48 +0200

    Update exercise_6.py

---
+++ Day 2/exercise_6.py
@@ -0,0 +1,13 @@
+"""
+Word Replacement: Replace all occurrences of a word in a given text with a new word.
+"""
+# Define the sentance
+sentence = "Hello Thapelo, how are you doing today?"
+# Define the word to be replaced
+word_to_replace = "Thapelo"
+# Define the new word
+new_word = "Lebohang"
+# Replace the word in the sentence
+sentence = sentence.replace(word_to_replace, new_word)
+# Print the modified sentence
+print(sentence)
