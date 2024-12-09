8876b22618c959595745817cef40fe54ab2b04a2
Thapelo Mahloko <mrtmahloko@gmail.com>
2024-12-09 11:58:00 +0200

    Create exercise_5.py

---
+++ DAY 4/exercise_5.py
@@ -0,0 +1,18 @@
+"""
+Even or Odd Game 
+Create a program that generates a random number between
+1 and 100. The user must guess if the number is even or 
+odd. After their guess, tell them whether they were 
+correct or not
+"""
+# Import the random module
+import random
+
+# Random numbers varible
+random_number = random.randint(1,100)
+
+# Check a if the random number is odd or even
+if random_number % 2 == 0:
+    print(f"The number {random_number} is even.")
+else:
+    print(f"The number {random_number} is odd.")
