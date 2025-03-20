9de87a6ee81f83c407711209e5095c970a825fc6
Thapelo Mahloko <mrtmahloko@gmail.com>
2025-03-20 22:13:02 +0200

    Update hangmang.py

--- Day 8/hangmang.py
+++ Day 8/hangmang.py
@@ -1,23 +1,98 @@
 import random
 
+shapes = [r''' 
+  +---+ 
+  |   | 
+  O   | 
+ /|\  | 
+ / \  | 
+      | 
+=========''',
+r'''
+  +---+ 
+  |   | 
+  O   | 
+ /|\  | 
+ /    | 
+      | 
+=========''',
+r'''
+  +---+ 
+  |   | 
+  O   | 
+ /|\  | 
+      | 
+      | 
+=========''',
+r'''
+  +---+ 
+  |   | 
+  O   | 
+ /|   | 
+      | 
+      | 
+=========''',
+r'''
+  +---+ 
+  |   | 
+  O   | 
+  |   | 
+      | 
+      | 
+=========''',
+r'''
+  +---+ 
+  |   | 
+  O   | 
+      | 
+      | 
+      | 
+=========''',
+r'''
+  +---+ 
+  |   | 
+      | 
+      | 
+      | 
+      | 
+==========''']
 
 # List of words
 word_list = ["apple", "banana", "cherry", "date", "elderberry"]
 
 # Randomly choosing a word from a list
 chosen_word = random.choice(word_list)
-print(chosen_word)
-print("_ " * len(chosen_word))
+place_holder = ""
+for position in range(len(chosen_word)):
+   place_holder += "_"
+print(place_holder)
 
-
-display = "_"
-attempts = 10
-
-while '_' in display and attempts > 0:
-    guess = input("Guess the letter in the word: ").lower()
+# Main Guessing Loop 
+game_over = False
+correct_letter = []
+game_lives = 7
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+    display = ""
+   # Check if the guessed letter is in the word
     for letter in chosen_word:
         if letter == guess:
-         display += letter
+            display += letter
+            correct_letter.append(guess)
+        elif letter in correct_letter:
+            display += letter
         else:
             display += "_"
+
     print(display)
+    
+    if guess not in chosen_word:
+        game_lives -= 1
+        
+    print(shapes[game_lives - 1])
+    if "_" not in display:
+        game_over = True
+        print("You win!")
+    elif game_lives == 0:
+        print("You lose the game!")
+        game_over = True
