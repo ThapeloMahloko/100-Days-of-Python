c043bb561e5b152d7f4fc34e0133b9854c890965
Thapelo Mahloko <mrtmahloko@gmail.com>
2025-06-11 20:12:31 +0200

    Update main.py

--- Day 10/Silent_Action/main.py
+++ Day 10/Silent_Action/main.py
@@ -7,7 +7,7 @@ bidder_info = {}
 
 while True:
     name = input("What is your name? ")
-    bidder_info[name] = "How much do you want to bid? R"
+    bidder_info[name] = input("How much do you want to bid? R ")
     other_bidder = input("Is there another bidder?(Yes(Y)/No(N)):\n")
 
     if other_bidder.lower() == "N".lower():
